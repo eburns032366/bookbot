5d423efcaf1da4cdbb29b2d932e9ee32b88aa74a
eburns032366 <eburns@coolidgeroad.us>
2025-06-11 16:40:28 -0400

    Completed bookbot project

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python3
 
 from stats import number_of_words
+from stats import letter_count
+from stats import sort_dict
+import sys
 
 def get_book_text(filepath):
-	return filepath.read()
+    return filepath.read()
 
 def main():
-	with open("books/frankenstein.txt") as filepath:
-		num_words = number_of_words(get_book_text(filepath))
-		print(f"{num_words} words found in the document")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print("------------ Word Count ------------")
+
+    with open(sys.argv[1]) as filepath:
+        num_words = number_of_words(get_book_text(filepath))
+        print(f"Found {num_words} total words")
+
+    print("------------ Character Count ------------")
+
+    with open(sys.argv[1]) as filepath:
+        count = letter_count(get_book_text(filepath))
+        sorted_count = sort_dict(count)
+        for key, value  in sorted_count.items():
+            if key.isalpha():
+                print(f"{key}: {value}")
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,11 @@
 def number_of_words(contents):
     return len(contents.split())
 
+def letter_count(contents):
+    letter_dict = {}
+    for letter in contents.lower():
+        letter_dict[letter] = letter_dict.get(letter,0) + 1
+    return letter_dict
+
+def sort_dict(letter_dict):
+    return dict(sorted(letter_dict.items()))
