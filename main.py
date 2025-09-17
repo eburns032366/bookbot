83d15cb7374d7bf4aa7c970df2cf32d484aaae1c
eburns <eburns@coolidgeroad.us>
2025-09-17 10:53:49 -0400

    end of project

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+import sys
+
 from stats import word_count 
-from stats import count_chars
-from stats import sort_on
+from stats import char_counts
+from stats import make_char_report
 
 def main():
-    wc = word_count()
-    cc = count_chars()
-    print(f"{wc} words found in the document")
-    for i in cc:
-        print(f"'{i}': {cc[i]}")
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        return
+    
+    book = sys.argv[1]
+    wc = word_count(book)
+    cc = char_counts(book)
+    mr = make_char_report(char_counts(book))
+
+    print(f"============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}...")
+    print(f"----------- Word Count ---------")
+    print(f"Found {wc} total words")
+    print(f"--------- Character Count ------")
+    
+    for i in mr:
+        ch = i["char"]
+        if ch.isalpha():
+            print(f"{ch}: {i['num']}")
     return
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -3,14 +3,14 @@ def get_book_text(file):
         file_contents = f.read()
     return file_contents
 
-def word_count():
-    book_text = get_book_text("books/frankenstein.txt")
+def word_count(book):
+    book_text = get_book_text(book)
     words = book_text.split()
     word_count = len(words)
     return word_count
 
-def count_chars():
-    book_text = get_book_text("books/frankenstein.txt")
+def char_counts(book):
+    book_text = get_book_text(book)
     char_count = {}
     for char in book_text:
         char = char.lower()
@@ -20,7 +20,13 @@ def count_chars():
             char_count[char] = 1
     return char_count
 
-def sort_on(count_chars):
-    cc = count_chars
-    return cc
+def make_char_report(char_counts):
+    items = []
+    for ch, n in char_counts.items():
+        items.append({"char": ch, "num": n})
+    items.sort(reverse=True, key=sort_helper)
+    return items
+
+def sort_helper(item):
+    return item["num"]
 
