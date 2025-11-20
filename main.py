46f94e00280aec089ee564804869aee26e1fb89a
eburns <eburns@coolidgeroad.us>
2025-11-20 15:43:50 -0500

    End of chapter 2

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from stats import get_word_count
+from stats import get_num_words, get_chars_dict
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    words = list(get_word_count(text))
-    print(f"Found {len(words)} total words")
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
+    
 
 def get_book_text(path):
     with open(path) as f:
